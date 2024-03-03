6048bef770a7eeccf1718f09af0a2f0a747bb57a
yuvraj0406 <152061508+yuvraj0406@users.noreply.github.com>
2024-03-04 00:17:08 +0530

    Add files via upload

---
+++ weather API/script.js
@@ -0,0 +1,11 @@
+// const apiKey = "36cf8691f871780879c064c6649ee16e";
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=delhi";
+
+// async function checkweather() {
+//     const response = await fetch(apiUrl + `&appid=${apiKey}`);
+//     var data = await response.json();
+//     console.log(data);
+// }
+// checkweather();
+
+// console.log("working")
